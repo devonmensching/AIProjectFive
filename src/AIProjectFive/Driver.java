ac53725c4d43a849002c13afb292d0ad0e8ef54b
devonmensching <devonmensching@gmail.com>
2017-05-03 17:10:54 -0400

    Methods for math formulas written.

--- src/AIProjectFive/Driver.java
+++ src/AIProjectFive/Driver.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 
 public class Driver {
 	
-	private static ArrayList<String> tableHeader;
-	private static int[][] table;
-	
+	private static ArrayList<String> attributes;
+	private static ArrayList<Instance> examples;
+
 
 	// main( String args[] ) - the driver for the program  
 	public static void main( String args[] ) throws FileNotFoundException 
@@ -21,13 +21,14 @@ public class Driver {
 		
 		// Read the information from the file 
 		ArrayList<String> fileData = readFile( fileName );
-		createTable( fileData );
+		createExamples( fileData );
 		
 		// Build the best decision tree that results from the training data
 		buildDecisionTree( );
 		
 		// Display or print the decision tree
 		displayTree( ); 
+		System.out.println(calcGain(0));
 	}
 	
 	// getFileName( ) - returns file name user has entered
@@ -49,7 +50,6 @@ public class Driver {
 		Scanner scanner = new Scanner(new BufferedReader(new FileReader(file)));
 		
 		// Create an ArrayList<String> of Strings representing the table data
-		tableHeader = new ArrayList<String>();
 		ArrayList<String> fileLines = new ArrayList<String>();
 		while( scanner.hasNext() )
 		{
@@ -63,6 +63,12 @@ public class Driver {
 	public static void buildDecisionTree( )
 	{
 		
+	}
+	
+	public static ArrayList<Node> decisionTreeLearning( int defaultValue )
+	{
+		ArrayList<Node> tree = new ArrayList<Node>();
+		return tree;
 	}
 	
 	// displayTree( ) - displays a decision tree on the monitor 
@@ -90,33 +96,127 @@ public class Driver {
 	}
 	
 	// createTable( ) - Creates the table from an ArrayList of file data
-	public static void createTable( ArrayList<String> fileData)
+	public static void createExamples( ArrayList<String> fileData)
 	{
 		// Create tableHeader ArrayList from file data
-		tableHeader = new ArrayList<String>();
+		attributes = new ArrayList<String>();
 		int count = 0;
 		while( !fileData.get( count ).equals("") )
 		{
-			tableHeader.add( fileData.get( count ) );
+			attributes.add( fileData.get( count ) );
 			count++;
 		}
 		
 		// Skip over blank line
 		count++;
 		
-		// Create int[][] table from file data
-		table = new int[fileData.size()-count][tableHeader.size()];
-		int tableRow = 0;
+		// Create ArrayList of examples 
+		examples = new ArrayList<Instance>();
 		while( count < fileData.size() )
 		{
-			String[] line = fileData.get( count ).split(",");
+			String[] lineData = fileData.get( count ).split(",");
+			int[] line = new int[lineData.length];
 			for(int i = 0; i < line.length; i++)
 			{
-				table[tableRow][i] = Integer.parseInt( line[i] );
+				line[i] = Integer.parseInt( lineData[i] );
 			}
-			tableRow++;
+			examples.add( new Instance( attributes, line ) );
 			count++;
 		}
 	}
 	
+	public static double calcI(  )
+	{
+		double p = 0.0;
+		double n = 0.0;
+		for(int i = 0; i < examples.size(); i++)
+		{
+			if(examples.get( i ).getClassification() == 1)
+			{
+				p++;
+			}
+			else
+			{
+				n++;
+			}
+			
+		}
+		double total = p + n;
+		return -(p/total)*(Math.log(p/total)/Math.log(2))-(n/total)*(Math.log(n/total)/Math.log(2));
+	}
+	
+	public static double calcIa( int attribute )
+	{
+		double p = 0.0;
+		double pTrue = 0.0;
+		double pFalse = 0.0;
+		double n = 0.0;
+		double nTrue = 0.0;
+		double nFalse = 0.0;
+		for(int i = 0; i < examples.size(); i++)
+		{
+			if(examples.get( i ).getValue( attribute ) == 1)
+			{
+				p++;
+				if(examples.get( i ).getClassification() == 1)
+				{
+					pTrue++;
+				}
+				else
+				{
+					pFalse++;
+				}
+			}
+			else
+			{
+				n++;
+				if(examples.get( i ).getClassification() == 1)
+				{
+					nTrue++;
+				}
+				else
+				{
+					nFalse++;
+				}
+			}
+			
+		}
+		if(p == 0 || n == 0)
+			return 0;
+		double total = p + n;
+		double first;
+		if(pTrue == 0 )
+		{
+			first = -(pFalse/p)*(Math.log(pFalse/p)/Math.log(2));
+		}
+		else if(pFalse == 0)
+		{
+			first = -(pTrue/p)*(Math.log(pTrue/p)/Math.log(2));
+		}
+		else
+		{
+			first = -(pTrue/p)*(Math.log(pTrue/p)/Math.log(2))-(pFalse/p)*(Math.log(pFalse/p)/Math.log(2));
+		}
+		
+		double second; 
+		if(nTrue == 0)
+		{
+			second = -(nFalse/n)*(Math.log(nFalse/n)/Math.log(2));
+		}
+		else if( nFalse == 0 )
+		{
+			second = -(nTrue/n)*(Math.log(nTrue/n)/Math.log(2));
+		}
+		else
+		{
+			second = -(nTrue/n)*(Math.log(nTrue/n)/Math.log(2))-(nFalse/n)*(Math.log(nFalse/n)/Math.log(2));
+		}
+		
+		return (p/total)* first + (n/total) * second;
+	}
+	
+	public static double calcGain( int attribute )
+	{
+		return calcI() - calcIa( attribute );
+	}
 }
